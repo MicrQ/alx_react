f06b8c27c9db7f975d66ada819bf69d39ffc371f
Abenet Gebre <mail.micrq@proton.me>
2024-12-25 17:55:16 +0300

    created Notification component

--- 0x00-react_introduction/task_0/dashboards/src/index.js
+++ 0x00-react_introduction/task_0/dashboards/src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+
+const root_notifications = ReactDOM.createRoot(
+  document.getElementById('root-notifications'));
+root_notifications.render(
+  <React.StrictMode>
+    <Notifications />
   </React.StrictMode>
 );
 
