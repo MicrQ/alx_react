98a88e1a1b5529c3bae62bc22950948f2389e01f
Abenet Gebre <mail.micrq@proton.me>
2024-12-26 10:32:36 +0300

    created basic tests for utils.js

---
+++ 0x00-react_introduction/task_2/dashboards/src/utils.test.js
@@ -0,0 +1,21 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
+
+describe('utils tests', () => {
+    test('getFullYear returns the correct year', () => {
+        const year = new Date().getFullYear();
+        expect(getFullYear()).toBe(year);
+    });
+
+    test('getFooterCopy returns the correct string when the argument is true', () => {
+        expect(getFooterCopy(true)).toBe('ALX');
+    });
+
+    test('getFooterCopy returns the correct string when the argument is false', () => {
+        expect(getFooterCopy(false)).toBe('ALX main dashboard');
+    });
+
+    test('getLatestNotification returns the expected string', () => {
+        expect(getLatestNotification())
+          .toBe('<strong>Urgent requirement</strong> - complete by EOD');
+    });
+});
